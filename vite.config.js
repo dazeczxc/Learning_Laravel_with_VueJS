b340fb4522b6fd74dd887473eea7e92fad555bf3
dazec <dasecojoshua@gmail.com>
2023-01-29 19:06:36 -0800

    Configured sanctrum configs

--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,8 @@ export default defineConfig({
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            '@': '/resources/js',
         },
     },
 });
