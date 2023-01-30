c9f421b22f95d081c05a3cdc651bb885248d239a
dazec <dasecojoshua@gmail.com>
2023-01-29 19:29:50 -0800

    updated welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,17 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>SPA Authentication using Laravel 9 Sanctum, Vue 3 and Vite - TechvBlogs</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        @vite('resources/css/app.css')
 
-       
+        @vite(['resources/js/app.js'])
     </head>
-    <body class="antialiased">
-        <div id="app"></div>
-
-        @vite('resources/js/app.js')
+    <body>
+        <div id="app">
+            <router-view></router-view>
+        </div>
     </body>
 </html>
